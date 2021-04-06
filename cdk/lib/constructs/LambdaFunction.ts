07bd41d6b68fd01056e843aff79ef0883c5588e8
Alexander Steppke <alex@steppke.dev>
2021-04-06 08:00:06 +0200

    try non singleton way of lambdainsightlayer

--- cdk/lib/constructs/LambdaFunction.ts
+++ cdk/lib/constructs/LambdaFunction.ts
@@ -1,5 +1,5 @@
 import {Construct, Stack} from '@aws-cdk/core';
-import {Tracing, Runtime, LayerVersion, ILayerVersion, IFunction} from '@aws-cdk/aws-lambda';
+import {Tracing, Runtime, LayerVersion, IFunction} from '@aws-cdk/aws-lambda';
 import {NodejsFunction, BundlingOptions} from '@aws-cdk/aws-lambda-nodejs'
 import {ManagedPolicy} from '@aws-cdk/aws-iam';
 
@@ -16,6 +16,11 @@ export class LambdaFunction extends Construct {
 
   constructor(scope: Construct, id: string, props: LambdaFunctionProps) {
     super(scope, id)
+    const lambdaInsightlayer = LayerVersion.fromLayerVersionArn(
+      scope,
+      'LambdaInsightsLayer',
+      `arn:aws:lambda:${Stack.of(this).region}:580247275435:layer:LambdaInsightsExtension:14`,
+    );
 
     this.lambdaFunction = new NodejsFunction(this, 'Resource', {
       entry: props.entry,
@@ -23,28 +28,11 @@ export class LambdaFunction extends Construct {
       runtime: props.runtime ?? Runtime.NODEJS_14_X,
       tracing: Tracing.ACTIVE,
       bundling: props.bundlingOptions,
-      layers: [this.getOrCreateLambdaInsightsLayer()],
+      layers: [lambdaInsightlayer],
     })
 
     this.lambdaFunction.role?.addManagedPolicy(
       ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLambdaInsightsExecutionRolePolicy'),
     );
-  }
-
-  public getOrCreateLambdaInsightsLayer() {
-    const uniqueid = 'LambdaInsightsLayer';
-    const stack = Stack.of(this);
-
-    let layer = stack.node.tryFindChild(uniqueid) as ILayerVersion
-
-    if (!layer) {
-      layer = LayerVersion.fromLayerVersionArn(
-        stack,
-        uniqueid,
-        `arn:aws:lambda:${Stack.of(this).region}:580247275435:layer:LambdaInsightsExtension:14`,
-      );
-    }
-
-    return layer
   }
 }
